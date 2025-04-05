44e2974976e6850835dda09a1c0514b58157cc17
Tyler Smith <tylertms@icloud.com>
2025-04-04 21:36:33 -0400

    Add validation layer support

---
+++ src/validation.c
@@ -0,0 +1,143 @@
+#include "validation.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+const char* validationLayers[] = {
+    "VK_LAYER_KHRONOS_validation",
+};
+
+#ifdef NODEBUG
+    const uint8_t enableValidationLayers = 0;
+#else
+    const uint8_t enableValidationLayers = 1;
+#endif
+
+int checkValidationLayerSupport() {
+    uint32_t layerCount;
+    vkEnumerateInstanceLayerProperties(&layerCount, 0);
+
+    VkLayerProperties* availableLayers = (VkLayerProperties*)malloc(layerCount * sizeof(VkLayerProperties));
+    vkEnumerateInstanceLayerProperties(&layerCount, availableLayers);
+    
+    for (int i = 0; i < ARRLEN(validationLayers); i++) {
+        int layerFound = 0;
+
+        for (int j = 0; j < layerCount; j++) {
+            if (strcmp(validationLayers[i], availableLayers[j].layerName) == 0) {
+                layerFound = 1;
+                break;
+            }
+        }
+
+        if (!layerFound) {
+            free(availableLayers);
+            return 0;
+        }
+    }
+
+    free(availableLayers);
+    return 1;
+}
+
+const char** getRequiredExtensions(uint32_t* extensionCount) {
+    const char** glfwExtensions = glfwGetRequiredInstanceExtensions(extensionCount);
+
+    uint32_t count = *extensionCount;
+
+    if (enableValidationLayers) {
+        count++;
+    }
+
+    const char** extensions = (const char**)malloc(sizeof(const char*) * count);
+
+    for (uint32_t i = 0; i < *extensionCount; ++i) {
+        extensions[i] = glfwExtensions[i];
+    }
+
+    if (enableValidationLayers) {
+        extensions[*extensionCount] = VK_EXT_DEBUG_UTILS_EXTENSION_NAME;
+    }
+
+    *extensionCount = count;
+    return extensions;
+}
+
+
+static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(
+    VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
+    VkDebugUtilsMessageTypeFlagsEXT messageType,
+    const VkDebugUtilsMessengerCallbackDataEXT *pCallbackData,
+    void *pUserData) {
+
+    printf("%s - %s: %s\n", severityString(messageSeverity), typeString(messageType), pCallbackData->pMessage);
+
+    return VK_FALSE;
+}
+
+void populateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT* createInfo) {
+    memset(createInfo, 0, sizeof(VkDebugUtilsMessengerCreateInfoEXT));
+
+    createInfo->sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
+    createInfo->messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
+    createInfo->messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
+    createInfo->pfnUserCallback = debugCallback;
+}
+
+void setupDebugMessenger(VKRT* vkrt) {
+    if (!enableValidationLayers) return;
+
+    VkDebugUtilsMessengerCreateInfoEXT createInfo;
+    populateDebugMessengerCreateInfo(&createInfo);
+
+    if (CreateDebugUtilsMessengerEXT(vkrt->instance, &createInfo, 0, &vkrt->debugMessenger) != VK_SUCCESS) {
+        printf("ERROR: Failed to set up debug messenger!");
+        exit(EXIT_FAILURE);
+    }
+}
+
+VkResult CreateDebugUtilsMessengerEXT(VkInstance instance, const VkDebugUtilsMessengerCreateInfoEXT* pCreateInfo, const VkAllocationCallbacks* pAllocator, VkDebugUtilsMessengerEXT* pDebugMessenger) {
+    PFN_vkCreateDebugUtilsMessengerEXT func = (PFN_vkCreateDebugUtilsMessengerEXT) vkGetInstanceProcAddr(instance, "vkCreateDebugUtilsMessengerEXT");
+    if (func != 0) {
+        return func(instance, pCreateInfo, pAllocator, pDebugMessenger);
+    } else {
+        return VK_ERROR_EXTENSION_NOT_PRESENT;
+    }
+}
+
+void DestroyDebugUtilsMessengerEXT(VkInstance instance, VkDebugUtilsMessengerEXT debugMessenger, const VkAllocationCallbacks* pAllocator) {
+    PFN_vkDestroyDebugUtilsMessengerEXT func = (PFN_vkDestroyDebugUtilsMessengerEXT) vkGetInstanceProcAddr(instance, "vkDestroyDebugUtilsMessengerEXT");
+    if (func != 0) {
+        func(instance, debugMessenger, pAllocator);
+    }
+}
+
+const char* severityString(VkDebugUtilsMessageSeverityFlagBitsEXT severity) {
+    switch (severity) {
+    case VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT:
+        return "VERBOSE";
+    case VK_DEBUG_UTILS_MESSAGE_SEVERITY_INFO_BIT_EXT:
+        return "INFO";
+    case VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT:
+        return "ERROR";
+    case VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT:
+        return "WARNING";
+    default:
+        return "OTHER";
+    }
+}
+
+const char* typeString(VkDebugUtilsMessageTypeFlagsEXT type) {
+    switch (type) {
+    case VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT:
+        return "GENERAL";
+    case VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT:
+        return "VALIDATION";
+    case VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT:
+        return "PERFORMANCE";
+    case VK_DEBUG_UTILS_MESSAGE_TYPE_DEVICE_ADDRESS_BINDING_BIT_EXT:
+        return "DEVICE ADDRESS BINDING";
+    default:
+        return "OTHER";
+    }
+}
---
+++ src/validation.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "vkrt.h"
+
+extern const char* validationLayers[];
+extern const uint8_t enableValidationLayers;
+
+int checkValidationLayerSupport();
+const char** getRequiredExtensions(uint32_t* extensionCount);
+
+void populateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT* createInfo);
+void setupDebugMessenger(VKRT* vkrt);
+
+VkResult CreateDebugUtilsMessengerEXT(VkInstance instance, const VkDebugUtilsMessengerCreateInfoEXT* pCreateInfo, const VkAllocationCallbacks* pAllocator, VkDebugUtilsMessengerEXT* pDebugMessenger);
+void DestroyDebugUtilsMessengerEXT(VkInstance instance, VkDebugUtilsMessengerEXT debugMessenger, const VkAllocationCallbacks* pAllocator);
+
+const char* severityString(VkDebugUtilsMessageSeverityFlagBitsEXT severity);
+const char* typeString(VkDebugUtilsMessageTypeFlagsEXT type);
