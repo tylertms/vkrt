316d08e27113f0a16775cac946a5cfe45f2bd794
Tyler Smith <tylertms@icloud.com>
2025-04-07 16:38:42 -0400

    More pipeline creation progress

--- src/pipeline.c
+++ src/pipeline.c
@@ -27,6 +27,49 @@ const char* readFile(const char* filename, size_t* fileSize) {
 }
 
 void createRayTracingPipeline(VKRT* vkrt) {
+    VkDescriptorSetLayoutBinding accelerationStructureLayoutBinding = {0};
+    accelerationStructureLayoutBinding.binding = 0;
+    accelerationStructureLayoutBinding.descriptorType = VK_DESCRIPTOR_TYPE_ACCELERATION_STRUCTURE_KHR;
+    accelerationStructureLayoutBinding.descriptorCount = 1;
+    accelerationStructureLayoutBinding.stageFlags = VK_SHADER_STAGE_RAYGEN_BIT_KHR;
+
+    VkDescriptorSetLayoutBinding resultImageLayoutBinding = {0};
+    resultImageLayoutBinding.binding = 1;
+    resultImageLayoutBinding.descriptorType = VK_DESCRIPTOR_TYPE_STORAGE_IMAGE;
+    resultImageLayoutBinding.descriptorCount = 1;
+    resultImageLayoutBinding.stageFlags = VK_SHADER_STAGE_RAYGEN_BIT_KHR;
+
+    VkDescriptorSetLayoutBinding uniformBufferBinding = {0};
+    uniformBufferBinding.binding = 2;
+    uniformBufferBinding.descriptorType = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
+    uniformBufferBinding.descriptorCount = 1;
+    uniformBufferBinding.stageFlags = VK_SHADER_STAGE_RAYGEN_BIT_KHR;
+
+    VkDescriptorSetLayoutBinding bindings[] = {
+        accelerationStructureLayoutBinding,
+        resultImageLayoutBinding,
+        uniformBufferBinding
+    };
+
+    VkDescriptorSetLayoutCreateInfo descriptorSetlayoutCreateInfo = {0};
+    descriptorSetlayoutCreateInfo.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_LAYOUT_CREATE_INFO;
+    descriptorSetlayoutCreateInfo.bindingCount = COUNT_OF(bindings);
+    descriptorSetlayoutCreateInfo.pBindings = bindings;
+    if (vkCreateDescriptorSetLayout(vkrt->device, &descriptorSetlayoutCreateInfo, NULL, &vkrt->descriptorSetLayout) != VK_SUCCESS) {
+        perror("ERROR: Failed to create descriptor set layout");
+        exit(EXIT_FAILURE);
+    }
+
+    VkPipelineLayoutCreateInfo pipelineLayoutInfo = {0};
+    pipelineLayoutInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
+    pipelineLayoutInfo.setLayoutCount = 0;
+    pipelineLayoutInfo.pushConstantRangeCount = 0;
+
+    if (vkCreatePipelineLayout(vkrt->device, &pipelineLayoutInfo, NULL, &vkrt->pipelineLayout) != VK_SUCCESS) {
+        perror("ERROR: Failed to create pipeline layout");
+        exit(EXIT_FAILURE);
+    }
+
     size_t rayGenLen;
     const char* rayGenCode = readFile("./shaders/rgen.spv", &rayGenLen);
     size_t closestHitLen;
@@ -44,11 +87,13 @@ void createRayTracingPipeline(VKRT* vkrt) {
     rayGenStageInfo.module = rayGenModule;
     rayGenStageInfo.pName = "main";
 
-    VkPipelineShaderStageCreateInfo closestHitStageInfo = {0};
-    closestHitStageInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
-    closestHitStageInfo.stage = VK_SHADER_STAGE_CLOSEST_HIT_BIT_KHR;
-    closestHitStageInfo.module = closestHitModule;
-    closestHitStageInfo.pName = "main";
+    VkRayTracingShaderGroupCreateInfoKHR rayGenShaderGroup = {0};
+    rayGenShaderGroup.sType = VK_STRUCTURE_TYPE_RAY_TRACING_SHADER_GROUP_CREATE_INFO_KHR;
+    rayGenShaderGroup.type = VK_RAY_TRACING_SHADER_GROUP_TYPE_GENERAL_KHR;
+    rayGenShaderGroup.generalShader = 0;
+    rayGenShaderGroup.closestHitShader = VK_SHADER_UNUSED_KHR;
+    rayGenShaderGroup.anyHitShader = VK_SHADER_UNUSED_KHR;
+    rayGenShaderGroup.intersectionShader = VK_SHADER_UNUSED_KHR;
 
     VkPipelineShaderStageCreateInfo missStageInfo = {0};
     missStageInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
@@ -56,45 +101,51 @@ void createRayTracingPipeline(VKRT* vkrt) {
     missStageInfo.module = missModule;
     missStageInfo.pName = "main";
 
+    VkRayTracingShaderGroupCreateInfoKHR missShaderGroup = {0};
+    missShaderGroup.sType = VK_STRUCTURE_TYPE_RAY_TRACING_SHADER_GROUP_CREATE_INFO_KHR;
+    missShaderGroup.type = VK_RAY_TRACING_SHADER_GROUP_TYPE_GENERAL_KHR;
+    missShaderGroup.generalShader = 1;
+    missShaderGroup.closestHitShader = VK_SHADER_UNUSED_KHR;
+    missShaderGroup.anyHitShader = VK_SHADER_UNUSED_KHR;
+    missShaderGroup.intersectionShader = VK_SHADER_UNUSED_KHR;
+    
+    VkPipelineShaderStageCreateInfo closestHitStageInfo = {0};
+    closestHitStageInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+    closestHitStageInfo.stage = VK_SHADER_STAGE_CLOSEST_HIT_BIT_KHR;
+    closestHitStageInfo.module = closestHitModule;
+    closestHitStageInfo.pName = "main";
+
+    VkRayTracingShaderGroupCreateInfoKHR closestHitShaderGroup = {0};
+    closestHitShaderGroup.sType = VK_STRUCTURE_TYPE_RAY_TRACING_SHADER_GROUP_CREATE_INFO_KHR;
+    closestHitShaderGroup.type = VK_RAY_TRACING_SHADER_GROUP_TYPE_TRIANGLES_HIT_GROUP_KHR;
+    closestHitShaderGroup.generalShader = VK_SHADER_UNUSED_KHR;
+    closestHitShaderGroup.closestHitShader = 2;
+    closestHitShaderGroup.anyHitShader = VK_SHADER_UNUSED_KHR;
+    closestHitShaderGroup.intersectionShader = VK_SHADER_UNUSED_KHR;
+
+
     VkPipelineShaderStageCreateInfo shaderStages[] = {
         rayGenStageInfo,
-        closestHitStageInfo,
-        missStageInfo
+        missStageInfo,
+        closestHitStageInfo
     };
 
-    VkDynamicState dynamicStates[] = {
-        VK_DYNAMIC_STATE_VIEWPORT,
-        VK_DYNAMIC_STATE_SCISSOR
+    VkRayTracingShaderGroupCreateInfoKHR shaderGroups[] = {
+        rayGenShaderGroup,
+        missShaderGroup,
+        closestHitShaderGroup
     };
-
-    VkPipelineDynamicStateCreateInfo dynamicState = {0};
-    dynamicState.sType = VK_STRUCTURE_TYPE_PIPELINE_DYNAMIC_STATE_CREATE_INFO;
-    dynamicState.dynamicStateCount = (uint32_t)COUNT_OF(dynamicStates);
-    dynamicState.pDynamicStates = dynamicStates;
-
-    VkPipelineLayoutCreateInfo pipelineLayoutInfo = {0};
-    pipelineLayoutInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
-    pipelineLayoutInfo.setLayoutCount = 0;
-    pipelineLayoutInfo.pushConstantRangeCount = 0;
-
-    if (vkCreatePipelineLayout(vkrt->device, &pipelineLayoutInfo, NULL, &vkrt->pipelineLayout) != VK_SUCCESS) {
-        perror("ERROR: Failed to create pipeline layout");
-        exit(EXIT_FAILURE);
-    }
 
     VkRayTracingPipelineCreateInfoKHR pipelineCreateInfo = {0};
     pipelineCreateInfo.sType = VK_STRUCTURE_TYPE_RAY_TRACING_PIPELINE_CREATE_INFO_KHR;
-    pipelineCreateInfo.maxPipelineRayRecursionDepth = 1;
-    pipelineCreateInfo.stageCount = 3;
+    pipelineCreateInfo.stageCount = COUNT_OF(shaderStages);
     pipelineCreateInfo.pStages = shaderStages;
-    pipelineCreateInfo.pDynamicState = &dynamicState;
+    pipelineCreateInfo.groupCount = COUNT_OF(shaderGroups);
+    pipelineCreateInfo.pGroups = shaderGroups;
+    pipelineCreateInfo.maxPipelineRayRecursionDepth = 1;
     pipelineCreateInfo.layout = vkrt->pipelineLayout;
-    pipelineCreateInfo.basePipelineHandle = VK_NULL_HANDLE;
-    pipelineCreateInfo.basePipelineIndex = -1;
 
-    PFN_vkCreateRayTracingPipelinesKHR pvkCreateRayTracingPipelinesKHR =
-    (PFN_vkCreateRayTracingPipelinesKHR)vkGetDeviceProcAddr(
-        vkrt->device, "vkCreateRayTracingPipelinesKHR");
+    PFN_vkCreateRayTracingPipelinesKHR pvkCreateRayTracingPipelinesKHR = (PFN_vkCreateRayTracingPipelinesKHR)vkGetDeviceProcAddr(vkrt->device, "vkCreateRayTracingPipelinesKHR");
 
     if (pvkCreateRayTracingPipelinesKHR(vkrt->device, VK_NULL_HANDLE, VK_NULL_HANDLE, 1, &pipelineCreateInfo, NULL, &vkrt->rayTracingPipeline) != VK_SUCCESS) {
         perror("ERROR: Failed to create ray tracing pipeline");
@@ -122,7 +173,7 @@ void createRenderPass(VKRT* vkrt) {
     colorAttachmentRef.layout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
 
     VkSubpassDescription subpass = {0};
-    subpass.pipelineBindPoint = VK_PIPELINE_BIND_POINT_RAY_TRACING_KHR;
+    subpass.pipelineBindPoint = VK_PIPELINE_BIND_POINT_GRAPHICS;
     subpass.colorAttachmentCount = 1;
     subpass.pColorAttachments = &colorAttachmentRef;
 
@@ -157,13 +208,15 @@ void createSyncObjects(VKRT* vkrt) {
     fenceInfo.sType = VK_STRUCTURE_TYPE_FENCE_CREATE_INFO;
     fenceInfo.flags = VK_FENCE_CREATE_SIGNALED_BIT;
 
-    VkResult imageAvailableSemaphoreResult = vkCreateSemaphore(vkrt->device, &semaphoreInfo, NULL, &vkrt->imageAvailableSemaphore);
-    VkResult renderFinishedSemaphoreResult = vkCreateSemaphore(vkrt->device, &semaphoreInfo, NULL, &vkrt->renderFinishedSemaphore);
-    VkResult inFlightFenceResult = vkCreateFence(vkrt->device, &fenceInfo, NULL, &vkrt->inFlightFence);
+    for (size_t i = 0; i < MAX_FRAMES_IN_FLIGHT; i++) {
+        VkResult imageAvailableSemaphoreResult = vkCreateSemaphore(vkrt->device, &semaphoreInfo, NULL, &vkrt->imageAvailableSemaphores[i]);
+        VkResult renderFinishedSemaphoreResult = vkCreateSemaphore(vkrt->device, &semaphoreInfo, NULL, &vkrt->renderFinishedSemaphores[i]);
+        VkResult inFlightFenceResult = vkCreateFence(vkrt->device, &fenceInfo, NULL, &vkrt->inFlightFences[i]);
 
-    if (imageAvailableSemaphoreResult != VK_SUCCESS || renderFinishedSemaphoreResult != VK_SUCCESS || inFlightFenceResult != VK_SUCCESS) {
-        perror("ERROR: Failed to create sync objects");
-        exit(EXIT_FAILURE);
+        if (imageAvailableSemaphoreResult != VK_SUCCESS || renderFinishedSemaphoreResult != VK_SUCCESS || inFlightFenceResult != VK_SUCCESS) {
+            perror("ERROR: Failed to create sync objects");
+            exit(EXIT_FAILURE);
+        }
     }
 }
 
