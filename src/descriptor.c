f844bfc37c9f75bd12ab15ed033b80fc81169b37
Tyler Smith <tylertms@icloud.com>
2025-05-17 23:15:09 -0700

    Update descriptor formatting

--- src/descriptor.c
+++ src/descriptor.c
@@ -1,6 +1,7 @@
 #include "descriptor.h"
-#include <stdlib.h>
+
 #include <stdio.h>
+#include <stdlib.h>
 
 void createDescriptorSetLayout(VKRT* vkrt) {
     VkDescriptorSetLayoutBinding accelerationStructureLayoutBinding = {0};
@@ -15,23 +16,22 @@ void createDescriptorSetLayout(VKRT* vkrt) {
     resultImageLayoutBinding.descriptorCount = 1;
     resultImageLayoutBinding.stageFlags = VK_SHADER_STAGE_RAYGEN_BIT_KHR;
 
-    VkDescriptorSetLayoutBinding uniformBufferBinding = {0};
-    uniformBufferBinding.binding = 2;
-    uniformBufferBinding.descriptorType = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
-    uniformBufferBinding.descriptorCount = 1;
-    uniformBufferBinding.stageFlags = VK_SHADER_STAGE_RAYGEN_BIT_KHR;
+    VkDescriptorSetLayoutBinding uniformBufferLayoutBinding = {0};
+    uniformBufferLayoutBinding.binding = 2;
+    uniformBufferLayoutBinding.descriptorType = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
+    uniformBufferLayoutBinding.descriptorCount = 1;
+    uniformBufferLayoutBinding.stageFlags = VK_SHADER_STAGE_RAYGEN_BIT_KHR;
 
     VkDescriptorSetLayoutBinding bindings[] = {
         accelerationStructureLayoutBinding,
         resultImageLayoutBinding,
-        uniformBufferBinding
-    };
+        uniformBufferLayoutBinding};
 
     VkDescriptorSetLayoutCreateInfo descriptorSetlayoutCreateInfo = {0};
     descriptorSetlayoutCreateInfo.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_LAYOUT_CREATE_INFO;
     descriptorSetlayoutCreateInfo.bindingCount = COUNT_OF(bindings);
     descriptorSetlayoutCreateInfo.pBindings = bindings;
-    
+
     if (vkCreateDescriptorSetLayout(vkrt->device, &descriptorSetlayoutCreateInfo, NULL, &vkrt->descriptorSetLayout) != VK_SUCCESS) {
         perror("ERROR: Failed to create descriptor set layout");
         exit(EXIT_FAILURE);
@@ -40,31 +40,30 @@ void createDescriptorSetLayout(VKRT* vkrt) {
 
 void createDescriptorPool(VKRT* vkrt) {
     VkDescriptorPoolSize poolSizes[] = {
-        { VK_DESCRIPTOR_TYPE_ACCELERATION_STRUCTURE_KHR, 1 },
-        { VK_DESCRIPTOR_TYPE_STORAGE_IMAGE, 1 },
-        { VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, 1 }
-    };
+        {VK_DESCRIPTOR_TYPE_ACCELERATION_STRUCTURE_KHR, 1},
+        {VK_DESCRIPTOR_TYPE_STORAGE_IMAGE, 1},
+        {VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, 1}};
 
-    VkDescriptorPoolCreateInfo poolInfo = {0};
-    poolInfo.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_POOL_CREATE_INFO;
-    poolInfo.poolSizeCount = COUNT_OF(poolSizes);
-    poolInfo.pPoolSizes = poolSizes;
-    poolInfo.maxSets = MAX_FRAMES_IN_FLIGHT;
+    VkDescriptorPoolCreateInfo descriptorPoolCreateInfo = {0};
+    descriptorPoolCreateInfo.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_POOL_CREATE_INFO;
+    descriptorPoolCreateInfo.poolSizeCount = COUNT_OF(poolSizes);
+    descriptorPoolCreateInfo.pPoolSizes = poolSizes;
+    descriptorPoolCreateInfo.maxSets = MAX_FRAMES_IN_FLIGHT;
 
-    if (vkCreateDescriptorPool(vkrt->device, &poolInfo, NULL, &vkrt->descriptorPool) != VK_SUCCESS) {
+    if (vkCreateDescriptorPool(vkrt->device, &descriptorPoolCreateInfo, NULL, &vkrt->descriptorPool) != VK_SUCCESS) {
         perror("ERROR: Failed to create descriptor pool");
         exit(EXIT_FAILURE);
     }
 }
 
 void createDescriptorSet(VKRT* vkrt) {
-    VkDescriptorSetAllocateInfo allocInfo = {0};
-    allocInfo.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_ALLOCATE_INFO;
-    allocInfo.descriptorPool = vkrt->descriptorPool;
-    allocInfo.descriptorSetCount = 1;
-    allocInfo.pSetLayouts = &vkrt->descriptorSetLayout;
+    VkDescriptorSetAllocateInfo descriptorSetAllocateInfo = {0};
+    descriptorSetAllocateInfo.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_ALLOCATE_INFO;
+    descriptorSetAllocateInfo.descriptorPool = vkrt->descriptorPool;
+    descriptorSetAllocateInfo.descriptorSetCount = 1;
+    descriptorSetAllocateInfo.pSetLayouts = &vkrt->descriptorSetLayout;
 
-    if (vkAllocateDescriptorSets(vkrt->device, &allocInfo, &vkrt->descriptorSet) != VK_SUCCESS) {
+    if (vkAllocateDescriptorSets(vkrt->device, &descriptorSetAllocateInfo, &vkrt->descriptorSet) != VK_SUCCESS) {
         perror("ERROR: Failed to allocate descriptor sets");
         exit(EXIT_FAILURE);
     }
@@ -73,19 +72,20 @@ void createDescriptorSet(VKRT* vkrt) {
     accelerationStructureInfo.sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET_ACCELERATION_STRUCTURE_KHR;
     accelerationStructureInfo.accelerationStructureCount = 1;
     accelerationStructureInfo.pAccelerationStructures = &vkrt->topLevelAccelerationStructure;
-    
+
     VkWriteDescriptorSet accelerationStructureWrite = {0};
     accelerationStructureWrite.sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET;
     accelerationStructureWrite.pNext = &accelerationStructureInfo;
     accelerationStructureWrite.dstSet = vkrt->descriptorSet;
+    accelerationStructureWrite.dstArrayElement = 0;
     accelerationStructureWrite.dstBinding = 0;
     accelerationStructureWrite.descriptorCount = 1;
     accelerationStructureWrite.descriptorType = VK_DESCRIPTOR_TYPE_ACCELERATION_STRUCTURE_KHR;
-    
+
     VkDescriptorImageInfo storageImageInfo = {0};
     storageImageInfo.imageView = vkrt->storageImageView;
     storageImageInfo.imageLayout = VK_IMAGE_LAYOUT_GENERAL;
-    
+
     VkWriteDescriptorSet storageImageWrite = {0};
     storageImageWrite.sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET;
     storageImageWrite.dstSet = vkrt->descriptorSet;
@@ -94,7 +94,7 @@ void createDescriptorSet(VKRT* vkrt) {
     storageImageWrite.descriptorType = VK_DESCRIPTOR_TYPE_STORAGE_IMAGE;
     storageImageWrite.descriptorCount = 1;
     storageImageWrite.pImageInfo = &storageImageInfo;
-    
+
     VkDescriptorBufferInfo sceneUniformInfo = {0};
     sceneUniformInfo.buffer = vkrt->uniformBuffer;
     sceneUniformInfo.offset = 0;
@@ -112,8 +112,7 @@ void createDescriptorSet(VKRT* vkrt) {
     VkWriteDescriptorSet writeDescriptorSets[] = {
         accelerationStructureWrite,
         storageImageWrite,
-        sceneUniformWrite
-    };
+        sceneUniformWrite};
 
     vkUpdateDescriptorSets(vkrt->device, COUNT_OF(writeDescriptorSets), writeDescriptorSets, 0, VK_NULL_HANDLE);
 }
