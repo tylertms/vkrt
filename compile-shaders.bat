73b7262a703c50a63a5fe6ecefe2d064418761d1
Tyler Smith <tylertms@icloud.com>
2025-05-18 22:40:16 -0700

    Build C bindings for ImGui

--- compile-shaders.bat
+++
@@ -1,4 +0,0 @@
-@echo OFF
-glslc .\src\shaders\main.rchit --target-env=vulkan1.4 -O -o .\shaders\rchit.spv
-glslc .\src\shaders\main.rmiss --target-env=vulkan1.4 -O -o .\shaders\rmiss.spv
-glslc .\src\shaders\main.rgen --target-env=vulkan1.4 -O -o .\shaders\rgen.spv
