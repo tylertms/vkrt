aef8c7b627d18bbbf279462a8a9ff533fc8f68d9
Tyler Smith <tylertms@icloud.com>
2025-05-22 21:15:31 -0400

    Fix small model glb loading

--- src/object.c
+++ src/object.c
@@ -18,100 +18,98 @@ void loadObject(VKRT* vkrt, const char* filename) {
     cgltf_options options = {0};
     cgltf_data* data = NULL;
 
-    cgltf_result res = cgltf_parse_file(&options, filename, &data);
-    if (res != cgltf_result_success) {
-        perror("ERROR: Failed to parse GLTF");
+    if (cgltf_parse_file(&options, filename, &data) != cgltf_result_success) {
+        fprintf(stderr, "ERROR: Failed to parse GLTF '%s'\n", filename);
+        exit(EXIT_FAILURE);
+    }
+    if (cgltf_load_buffers(&options, data, filename) != cgltf_result_success) {
+        cgltf_free(data);
+        fprintf(stderr, "ERROR: Failed to load buffers for '%s'\n", filename);
         exit(EXIT_FAILURE);
     }
 
-    res = cgltf_load_buffers(&options, data, filename);
-    if (res != cgltf_result_success) {
-        cgltf_free(data);
-        return;
-    }
-
-    size_t numVertices = 0;
-    size_t numIndices = 0;
-
+    size_t numVertices = 0, numIndices = 0;
     for (size_t m = 0; m < data->meshes_count; m++) {
         cgltf_mesh* mesh = &data->meshes[m];
         for (size_t p = 0; p < mesh->primitives_count; p++) {
             cgltf_primitive* prim = &mesh->primitives[p];
-            cgltf_accessor* posAccessor = NULL;
-            for (size_t a = 0; a < prim->attributes_count; a++) {
-                if (prim->attributes[a].type == cgltf_attribute_type_position) {
-                    posAccessor = prim->attributes[a].data;
-                    break;
-                }
-            }
-            if (!posAccessor)
+            if (prim->type != cgltf_primitive_type_triangles || !prim->indices)
                 continue;
-            numVertices += posAccessor->count;
-            numIndices += prim->indices ? prim->indices->count : 0;
+            cgltf_accessor* posAcc = NULL;
+            cgltf_accessor* normAcc = NULL;
+            for (size_t a = 0; a < prim->attributes_count; a++) {
+                if (prim->attributes[a].type == cgltf_attribute_type_position)
+                    posAcc = prim->attributes[a].data;
+                else if (prim->attributes[a].type == cgltf_attribute_type_normal)
+                    normAcc = prim->attributes[a].data;
+            }
+            if (!posAcc || !normAcc)
+                continue;
+            numVertices += posAcc->count;
+            numIndices += prim->indices->count;
         }
     }
 
     Vertex* vertices = malloc(numVertices * sizeof(Vertex));
     uint32_t* indices = malloc(numIndices * sizeof(uint32_t));
-
-    size_t vertexBase = 0;
-    size_t indexBase = 0;
+    size_t vertexBase = 0, indexBase = 0;
 
     for (size_t m = 0; m < data->meshes_count; m++) {
         cgltf_mesh* mesh = &data->meshes[m];
         for (size_t p = 0; p < mesh->primitives_count; p++) {
             cgltf_primitive* prim = &mesh->primitives[p];
-            cgltf_accessor* posAccessor = NULL;
-            cgltf_accessor* normAccessor = NULL;
-            for (size_t a = 0; a < prim->attributes_count; a++) {
-                if (prim->attributes[a].type == cgltf_attribute_type_position)
-                    posAccessor = prim->attributes[a].data;
-                else if (prim->attributes[a].type == cgltf_attribute_type_normal)
-                    normAccessor = prim->attributes[a].data;
-            }
-            if (!posAccessor || !normAccessor)
+            if (prim->type != cgltf_primitive_type_triangles || !prim->indices)
                 continue;
 
-            uint8_t* rawV = (uint8_t*)posAccessor->buffer_view->buffer->data;
-            rawV += posAccessor->buffer_view->offset + posAccessor->offset;
-            size_t vStride = posAccessor->stride;
+            cgltf_accessor* posAcc = NULL;
+            cgltf_accessor* normAcc = NULL;
+            for (size_t a = 0; a < prim->attributes_count; a++) {
+                if (prim->attributes[a].type == cgltf_attribute_type_position)
+                    posAcc = prim->attributes[a].data;
+                else if (prim->attributes[a].type == cgltf_attribute_type_normal)
+                    normAcc = prim->attributes[a].data;
+            }
+            if (!posAcc || !normAcc)
+                continue;
 
-            uint8_t* rawN = (uint8_t*)normAccessor->buffer_view->buffer->data;
-            rawN += normAccessor->buffer_view->offset + normAccessor->offset;
-            size_t nStride = normAccessor->stride;
-
-            for (size_t i = 0; i < posAccessor->count; i++) {
-                float* pSrc = (float*)(rawV + i * vStride);
-                float* nSrc = (float*)(rawN + i * nStride);
-                Vertex* V = vertices + vertexBase + i;
-                V->position[0] = pSrc[0];
-                V->position[1] = pSrc[1];
-                V->position[2] = -pSrc[2];
-                V->normal[0] = -nSrc[0];
-                V->normal[1] = -nSrc[1];
-                V->normal[2] = -nSrc[2];
+            for (size_t i = 0; i < posAcc->count; i++) {
+                float pos[3], norm[3];
+                cgltf_accessor_read_float(posAcc, i, pos, 3);
+                cgltf_accessor_read_float(normAcc, i, norm, 3);
+                Vertex* V = &vertices[vertexBase + i];
+                V->position[0] = pos[0];
+                V->position[1] = pos[1];
+                V->position[2] = -pos[2];
+                V->normal[0] = -norm[0];
+                V->normal[1] = -norm[1];
+                V->normal[2] = -norm[2];
             }
 
-            if (prim->indices) {
-                cgltf_accessor* idxAccessor = prim->indices;
-                uint8_t* rawI = (uint8_t*)idxAccessor->buffer_view->buffer->data;
-                rawI += idxAccessor->buffer_view->offset + idxAccessor->offset;
-                size_t iStride = idxAccessor->stride;
-                for (size_t i = 0; i < idxAccessor->count; i++) {
-                    uint32_t raw = 0;
-                    if (idxAccessor->component_type == cgltf_component_type_r_16) {
-                        uint16_t v;
-                        memcpy(&v, rawI + i * iStride, sizeof(uint16_t));
-                        raw = v;
-                    } else {
-                        memcpy(&raw, rawI + i * iStride, sizeof(uint32_t));
-                    }
-                    indices[indexBase + i] = raw + (uint32_t)vertexBase;
+            cgltf_accessor* idxAcc = prim->indices;
+            size_t idxCount = idxAcc->count;
+            uint8_t* raw = (uint8_t*)idxAcc->buffer_view->buffer->data + idxAcc->buffer_view->offset + idxAcc->offset;
+            size_t stride = idxAcc->stride;
+            for (size_t i = 0; i < idxCount; i++) {
+                uint32_t idxValue;
+                if (idxAcc->component_type == cgltf_component_type_r_16u) {
+                    uint16_t tmp;
+                    memcpy(&tmp, raw + i * stride, sizeof(tmp));
+                    idxValue = tmp;
+                } else if (idxAcc->component_type == cgltf_component_type_r_32u) {
+                    uint32_t tmp;
+                    memcpy(&tmp, raw + i * stride, sizeof(tmp));
+                    idxValue = tmp;
+                } else {
+                    fprintf(stderr,
+                            "ERROR: Unsupported index component type %u\n",
+                            idxAcc->component_type);
+                    exit(EXIT_FAILURE);
                 }
-                indexBase += idxAccessor->count;
+                indices[indexBase + i] = idxValue + (uint32_t)vertexBase;
             }
 
-            vertexBase += posAccessor->count;
+            indexBase += idxCount;
+            vertexBase += posAcc->count;
         }
     }
 
@@ -119,13 +117,15 @@ void loadObject(VKRT* vkrt, const char* filename) {
     vkrt->indexCount = (uint32_t)numIndices;
 
     vkrt->vertexBufferDeviceAddress = createBufferFromHostData(
-        vkrt, vertices, numVertices * sizeof(Vertex),
+        vkrt,
+        vertices, numVertices * sizeof(Vertex),
         VK_BUFFER_USAGE_VERTEX_BUFFER_BIT | VK_BUFFER_USAGE_SHADER_DEVICE_ADDRESS_BIT | VK_BUFFER_USAGE_ACCELERATION_STRUCTURE_BUILD_INPUT_READ_ONLY_BIT_KHR | VK_BUFFER_USAGE_STORAGE_BUFFER_BIT,
         &vkrt->vertexBuffer,
         &vkrt->vertexBufferMemory);
 
     vkrt->indexBufferDeviceAddress = createBufferFromHostData(
-        vkrt, indices, numIndices * sizeof(uint32_t),
+        vkrt,
+        indices, numIndices * sizeof(uint32_t),
         VK_BUFFER_USAGE_INDEX_BUFFER_BIT | VK_BUFFER_USAGE_SHADER_DEVICE_ADDRESS_BIT | VK_BUFFER_USAGE_ACCELERATION_STRUCTURE_BUILD_INPUT_READ_ONLY_BIT_KHR | VK_BUFFER_USAGE_STORAGE_BUFFER_BIT,
         &vkrt->indexBuffer,
         &vkrt->indexBufferMemory);
