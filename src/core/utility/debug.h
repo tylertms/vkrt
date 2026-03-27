f4197da21c4bcfa523b7420ecfa27e4002e389c1
Tyler Smith <tylertms@icloud.com>
2026-03-27 12:28:56 -0400

    use gnu_printf log macro on mingw

--- src/core/utility/debug.h
+++ src/core/utility/debug.h
@@ -8,7 +8,12 @@ extern "C" {
 #endif
 
 #if defined(__GNUC__) || defined(__clang__)
-void vkrtLogLine(FILE* stream, const char* level, const char* format, ...) __attribute__((format(printf, 3, 4)));
+#if defined(__MINGW32__) || defined(__MINGW64__)
+#define VKRT_PRINTF_ATTRIBUTE(formatIndex, firstArg) __attribute__((format(gnu_printf, formatIndex, firstArg)))
+#else
+#define VKRT_PRINTF_ATTRIBUTE(formatIndex, firstArg) __attribute__((format(printf, formatIndex, firstArg)))
+#endif
+void vkrtLogLine(FILE* stream, const char* level, const char* format, ...) VKRT_PRINTF_ATTRIBUTE(3, 4);
 #else
 void vkrtLogLine(FILE* stream, const char* level, const char* format, ...);
 #endif
@@ -42,6 +47,10 @@ void vkrtSetInfoLoggingEnabled(int enabled);
 #define LOG_ERROR(...) VKRT_LOG_LINE(stderr, "[ERROR]", __VA_ARGS__)
 
 uint64_t getMicroseconds(void);
+
+#if defined(__GNUC__) || defined(__clang__)
+#undef VKRT_PRINTF_ATTRIBUTE
+#endif
 
 #ifdef __cplusplus
 }
